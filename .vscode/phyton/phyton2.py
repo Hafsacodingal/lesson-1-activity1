23150ffc7f3f70a9f3de8decfab874dd7a0625d3
Hafsacodingal <hafsayaqubonnet@gmail.com>
2026-05-09 15:30:17 +0500

    Data types Type Casting

---
+++ .vscode/phyton/phyton2.py
@@ -0,0 +1,21 @@
+name ="Hafsa"
+Age =18
+is_student="true"
+weight=49.23
+
+print("name;",name )
+print("Data type of name is", type(name))
+print("Age;",Age )
+print("Data type of Age is", type(Age))
+print("is_student;", is_student)
+print("Data type of is_student is", type(is_student))
+print("weight;",weight )
+print("Data type of weight is", type(weight))
+
+print("\n After casting....")
+Age= str(Age)
+print(Age)
+print("Data type of Age is",type(Age))
+weight=int(weight)
+print(weight)
+print("Data type of weight is",type(weight))
