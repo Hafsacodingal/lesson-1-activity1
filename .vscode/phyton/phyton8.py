b103c43f786e7d119948092b5bfabe480d945283
Hafsacodingal <hafsayaqubonnet@gmail.com>
2026-05-10 15:50:42 +0500

    Exam Eligibility

---
+++ .vscode/phyton/phyton8.py
@@ -0,0 +1,5 @@
+attendance= float (input("Enter your attandance percentage:"))
+if attendance >=75:
+    print("You are eliible for the exam.")
+else:
+    print("You are not eliible for the exam.")
