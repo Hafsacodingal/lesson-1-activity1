48f45e76e86afa2f7b2e368702635920b2554fcc
Hafsacodingal <hafsayaqubonnet@gmail.com>
2026-05-10 17:35:05 +0500

    Number swapping

---
+++ .vscode/phyton/phyton9.py
@@ -0,0 +1,11 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+c = input("Enter third number: ")
+
+# swapping
+a, b, c = c, a, b
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
