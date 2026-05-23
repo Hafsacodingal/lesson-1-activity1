1b9c2e12c7f2a6147803cf45a0a34dec7698b58a
Hafsacodingal <hafsayaqubonnet@gmail.com>
2026-05-23 23:01:04 +0500

    Fibonacci Series

---
+++ .vscode/phyton/phyton17.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    a = 0
+    b = 1
+
+    for i in range(n):
+        print(a)
+        temp = a + b
+        a = b
+        b = temp
+
+n = int(input("Enter number of terms: "))
+fibonacci(n)
