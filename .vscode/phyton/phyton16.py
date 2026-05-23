b2c745dcd6631aa64652c4c02f80ba16f6e2d4ff
Hafsacodingal <hafsayaqubonnet@gmail.com>
2026-05-23 22:59:24 +0500

    Armstrong Number

---
+++ .vscode/phyton/phyton16.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+
+temp = num
+sum = 0
+
+# count digits
+digits = len(str(num))
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + (digit ** digits)
+    temp = temp // 10
+
+if sum == num:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
