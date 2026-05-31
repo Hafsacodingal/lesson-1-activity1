d42d26225255b2f491af79a585ea35709523a1d0
Hafsacodingal <hafsayaqubonnet@gmail.com>
2026-05-31 16:09:34 +0500

    Parrot Bird

---
+++ .vscode/phyton/phyton25.py
@@ -0,0 +1,26 @@
+#Parrot Bird
+class Parrot:
+    species="bird" 
+    def __init__(self,name,age):
+       self.name=name
+       self.age=age
+blu=Parrot("Blu",10)
+woo=Parrot("Woo",15)
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+print("{} is {} years old".format(blu.name,blu.age))
+print("{} is {} years old".format(woo.name,woo.age))
+#Parrot Bird-II
+class Parrot:
+    species="bird" 
+    def __init__(self,name,age):
+       self.name=name
+       self.age=age
+    def sing(self,song):
+        return"{} sings {}".format(self.name,song)
+    def dance(self):
+        return"{} is now dancing".format(self.name)
+blu=Parrot("Blu",10)
+print(blu.sing("'Happy'"))
+print(blu.dance())
+
